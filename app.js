cfd1bd69744f5f6bd65ecd290be4fa3dca904bc7
rosarymeditation <mr.nnamdinwosu@gmail.com>
2023-09-21 18:50:40 +0100

    Fixed app.js and mongo url error

--- app.js
+++ app.js
@@ -50,18 +50,27 @@ const cookieExtractor = (res) => {
     token = req.cookies[ACCESS_TOKEN];
   } else return token;
 };
-const mongoString =
-  "mongodb+srv://nnamdi4nwosu:6xVUznWAtJ3mxNfy@cluster0.kbvmdmk.mongodb.net/test";
+const mongoString = process.env.DATABASE_URL;
 const mongoose = require("mongoose");
-mongoose.connect(mongoString);
-const database = mongoose.connection;
-database.on("error", (error) => {
-  console.log(error);
-});
+mongoose.set("strictQuery", false);
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DATABASE_URL);
+    console.log(`Mongo connected : ${conn.connection.host}`);
+  } catch (err) {
+    console.log(err);
+    process.exit;
+  }
+};
+// mongoose.connect(mongoString);
+// const database = mongoose.connection;
+// database.on("error", (error) => {
+//   console.log(error);
+// });
 
-database.once("connected", () => {
-  console.log("Database Connected");
-});
+// database.once("connected", () => {
+//   console.log("Database Connected");
+// });
 // Require our routes into the application.
 const refPath = "./server/routes/";
 
@@ -78,41 +87,18 @@ require(`${refPath}audio`)(app);
 
 app.use((err, req, res, next) => res.json(err));
 
-app.set("port", process.env.PORT || 8001);
-
-const server = app.listen(app.get("port"), function () {
-  console.log("Server started on port " + app.get("port"));
+//app.set("port", process.env.PORT || 8001);
+connectDB().then(() => {
+  app.listen(process.env.PORT || 8001, () => {
+    console.log(`Listening to port ${process.env.PORT || 8001}`);
+  });
 });
+
+// const server = app.listen(app.get("port"), function () {
+//   console.log("Server started on port " + app.get("port"));
+// });
 app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
 
 module.exports = app;
-
-// const express = require("express");
-// const cors = require("cors");
-// require("dotenv").config();
-// const mongoString = process.env.DATABASE_URL;
-// const mongoose = require("mongoose");
-// mongoose.connect(mongoString);
-// const database = mongoose.connection;
-// const app = express();
-
-// app.use(express.json());
-// app.use(cors());
-// app.listen(process.env.PORT || 3000, () => {
-//   console.log(`Server Started at ${3000}`);
-// });
-// //app.set("port", process.env.PORT || 8000);
-
-// const refPath = "./server/routes/";
-// require(`${refPath}testimony`)(app);
-// require(`${refPath}user`)(app);
-// require(`${refPath}feed`)(app);
-// require(`${refPath}feedComment`)(app);
-// require(`${refPath}feedStatus`)(app);
-// require(`${refPath}prayer`)(app);
-// require(`${refPath}language`)(app);
-// require(`${refPath}suggestion`)(app);
-// require(`${refPath}prayerCatholic`)(app);
-// require(`${refPath}audio`)(app);
