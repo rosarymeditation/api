33fe245ef637441bbb84b7da94cf0c19062ea3a8
Uloma Okenyi <cyndibilo@gmail.com>
2023-09-30 04:42:53 +0000

    added  user create role and forgot password to user controller API

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,6 @@
 // const PostCode = require("../models").PostCode;
-// const Query = new require("../queries/crud");
-// const validate = require("../validations/validation");
+const Query = new require("../queries/crud");
+const validate = require("../validations/validation");
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const {
@@ -20,6 +20,65 @@ const User = require("../models/user");
 const Role = require("../models/role");
 
 module.exports = {
+  UsersignUp: async (req, res) => {
+    try {
+      const { password, email, firstname, lastname } = req.body;
+      const findEmail = await User.findOne({ email });
+
+      if (findEmail) {
+        return res.status(VALIDATION_ERROR).send({
+          error: true,
+          message: "Email already exist",
+        });
+      }
+      bcrypt.hash(password, 10, async (err, hash) => {
+        if (err) {
+          return res
+            .status(SERVER_ERROR)
+            .send({ message: "Error", error: true });
+        } else {
+          const dataObj = new User({
+            email: email,
+            password: hash,
+            role: "6530595ad24dd0acc26c71e1",
+            firstname: CapitalizeFirstLetter(firstname),
+            lastname: CapitalizeFirstLetter(lastname),
+            email,
+          });
+          try {
+            const data = await dataObj.save();
+            const token = jwt.sign(
+              {
+                id: data.id,
+                email: email,
+                firstname: firstname,
+                lastname: lastname,
+              },
+              secret,
+              {
+                expiresIn: "7000d",
+              }
+            );
+            return res.status(OK).send({
+              error: false,
+              token: token,
+              userId: data.id,
+            });
+          } catch (err) {
+            console.log(err);
+            return res.status(SERVER_ERROR).send({
+              error: true,
+              message: err,
+            });
+          }
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+
+    //return res.status(VALIDATION_ERROR).send({ message: error, error: true });
+  },
   signUp: async (req, res) => {
     try {
       const { password, email, firstname, lastname } = req.body;
@@ -308,7 +367,53 @@ module.exports = {
 
     //return res.status(VALIDATION_ERROR).send({ message: error, error: true });
   },
+  changePassword: async (req, res) => {
+    try {
+      const { email, code, password } = req.body;
+      const findUser = await User.findOne({ email, verifyCode: code });
 
+      if (!findUser) {
+        return res.status(VALIDATION_ERROR).send({
+          error: true,
+          message: "Code does not exist",
+        });
+      }
+      bcrypt.hash(password, 10, async (err, hash) => {
+        if (err) {
+          return res
+            .status(SERVER_ERROR)
+            .send({ message: "Error", error: true });
+        } else {
+          const options = { new: true };
+          console.log("0300404040040i0000------00595959");
+          const user = await User.findByIdAndUpdate(
+            findUser._id,
+            { password: hash, verifyCode: "" },
+            options
+          );
 
+          return res.status(OK).send({ message: "Successful", error: false });
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
 
+    //return res.status(VALIDATION_ERROR).send({ message: error, error: true });
+  },
+
+  createRole: async (req, res) => {
+    try {
+      const { name } = req.body;
+      const data = Role({
+        name: name,
+      });
+      await data.save();
+
+      return res.status(OK).send({ error: false });
+    } catch (err) {
+      console.log(err);
+      return res.status(OK).send({ error: true });
+    }
+  },
 };
