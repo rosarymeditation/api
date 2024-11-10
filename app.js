1033ba39cd6f900893ddc51b942952ec84062d2b
Nnamdi <mr.nnamdinwosu@gmail.com>
2024-11-10 12:13:01 +0000

    removed sentry profile

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const Sentry = require("@sentry/node");
-const ProfilingIntegration = require("@sentry/profiling-node");
-const path = require("path");
+
 const bodyParser = require("body-parser");
 
 require("dotenv").config();
