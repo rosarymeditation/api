8476617c151d34a0500498ed1cc7c624c56b696a
Nnamdi <mr.nnamdinwosu@gmail.com>
2025-03-02 18:47:58 +0000

    Added spanish translation

--- server/routes/translate.js
+++
@@ -1,7 +0,0 @@
-const controller = require("../controllers/translate");
-const { rootUrl } = require("../utility/constants");
-const { auth, upload } = require("../utility/global");
-
-module.exports = (app) => {
-  app.post(rootUrl("translate"), controller.convert);
-};
